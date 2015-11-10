869f8887d77cdfdaeb2cc0117bcdd56f159dbb54
Isizukuri <ragnarok49@gmail.com>
2015-11-10 21:53:20 +0200

    Some jQuery added.

--- livaravaTest/testapp/static/scripts/script.js
+++ livaravaTest/testapp/static/scripts/script.js
@@ -1,3 +1,17 @@
-$('document').ready(function(){
-	
+$(document).ready(function() {
+    $(".year_input").hide();
+    $(".author_input").hide();
+    $('.year_toggle').click(function() {
+        $(".year_select").fadeOut(400, function() {
+            $(".year_input").fadeIn(400, function() {})
+        });
+        $('.year_toggle').fadeOut(400);
+    });
+
+    $('.author_toggle').click(function() {
+        $(".author_select").fadeOut(400, function() {
+            $(".author_input").fadeIn(400, function() {})
+        });
+        $('.author_toggle').fadeOut(400);
+    });
 });
--- livaravaTest/testapp/views.py
+++ livaravaTest/testapp/views.py
@@ -22,6 +22,8 @@ def names(request):
     verses = Verse.category().instance.all()
     authors = Author.category().instance.all()
     years = Year.category().instance.all()
+    auth_names = [author.name for author in Author.category().instance.all()]
+    year_list = [year.year for year in Year.category().instance.all()]
     if request.method == 'GET':
         return render(request, 'jinja2/names.html', {'verses': verses,
                                                      'authors': authors,
@@ -30,9 +32,17 @@ def names(request):
         if (request.POST.get('name') is not None) and (request.POST.get('text') is not None):
             verse = Verse(name=unicode(request.POST['name']), text=unicode(request.POST['text'])).save()
             if request.POST.get('author'):
-                verse.author.connect(Author.index.get(name=request.POST['author']))
+            	if request.POST.get('author') in auth_names:
+                	verse.author.connect(Author.index.get(name=request.POST['author']))
+                else:
+                	Author(name=unicode(request.POST['author'])).save()
+                	verse.author.connect(Author.index.get(name=request.POST['author']))
             if request.POST.get('year'):
-                verse.year.connect(Year.index.get(year=int(request.POST['year'])))
+            	if request.POST.get('year') in year_list:
+                	verse.year.connect(Year.index.get(year=int(request.POST['year'])))
+                else:
+                	Year(year=unicode(request.POST['year'])).save()
+                	verse.year.connect(Year.index.get(year=int(request.POST['year'])))               
         verses = Verse.category().instance.all()
         authors = Author.category().instance.all()
         years = Year.category().instance.all()
